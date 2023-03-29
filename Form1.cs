861ef1adee0a9a01e6073025567f80091466d2e2
AGiuroiu <agiurou@gmail.com>
2023-03-29 22:12:11 +0300

    aproape gata

---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.UserInputText.Text.Length < this.UserInputText.SelectionStart + 1)
+                return;
+                var selectionStart = this.UserInputText.SelectionStart;
+            this.UserInputText.Text = this.UserInputText.Text.Remove(this.UserInputText.SelectionStart, 1);
+            
+            this.UserInputText.Text = this.UserInputText.Text.Insert(this.UserInputText.SelectionStart, value);
+            this.UserInputText.SelectionStart = selectionStart + value.Length;
+            this.UserInputText.SelectionLength = 0;
+            FocusInputText();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            InsertTextValue(":");
+            FocusInputText();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("7");
+            FocusInputText();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("4");
+            FocusInputText();
+        }
+
+        private void CEButton_Click(object sender, EventArgs e)
+        {
+            this.UserInputText.Text = string.Empty;
+            FocusInputText();
+        }
+
+        private void ZeroButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("0");
+            FocusInputText();
+        }
+
+        private void OneButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("1");
+            FocusInputText();
+        }
+
+        private void TwoButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("2");
+            FocusInputText();
+        }
+
+        private void ThreeButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("3");
+            FocusInputText();
+        }
+
+        private void FiveButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("5");
+            FocusInputText();
+        }
+
+        private void SixButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("6");
+            FocusInputText();
+        }
+
+        private void EightButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("8");
+            FocusInputText();
+        }
+
+        private void NineButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("9");
+            FocusInputText();
+        }
+        private void InsertTextValue(string value)
+        {
+
+            var selectionStart = this.UserInputText.SelectionStart;
+            this.UserInputText.Text = this.UserInputText.Text.Insert(this.UserInputText.SelectionStart, value);
+            this.UserInputText.SelectionStart = selectionStart + value.Length;
+            this.UserInputText.SelectionLength = 0;
+        }
+
+        private void DotButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue(".");
+            FocusInputText();
+        }
+
+        private void EqualButton_Click(object sender, EventArgs e)
+        {
+            CalculateEquation();
+        }
+
+        private void PlusButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("+");
+            FocusInputText();
+        }
+
+        private void MinusButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("-");
+            FocusInputText();
+        }
+
+        private void MultiplyButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("*");
+            FocusInputText();
+        }
+        private void CalculateEquation()
+        {
+            throw new NotImplementedException();
+        }
+        private void FocusInputText()
+        {
+        this.UserInputText.Focus();
+        }
+
+        private void CButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
